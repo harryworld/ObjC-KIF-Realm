91db645a32a713e7f08f2cb052ad5535047b8c19
STAY REAL <stayreal@STAYs-MacBook-Air.local>
2015-05-21 18:37:59 +0800

    Initial Commit

---
+++ ObjC-KIF-Realm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjC-KIF-Realm
+//
+//  Created by STAY REAL on 21/5/15.
+//  Copyright (c) 2015 STAY REAL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
